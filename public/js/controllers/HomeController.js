fa7f745640ffe77077c24857fd409ec9a28fbcd7
Coleman Kelleghan <imdesigner2012@gmail.com>
2017-05-07 18:26:54 -0600

    Testing

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -14,37 +14,12 @@ thisApp
     $scope.articles = [];
     $scope.orderBy = 'date';
 
-    // var ellipsizeTextBox = function(element) {
-
-      // var el = document.getElementsByClassName(element);
-      // console.log("checking", el);
-
-
-      // for (var i = 0; i < el.length; i++) {
-      //   var keep = el[i].innerHTML;
-      //
-      //   while(el[i].scrollHeight > el[i].offsetHeight) {
-      //     el[i].innerHTML = keep;
-      //     el[i].innerHTML = el[i].innerHTML.substring(0, el[i].innerHTML.length-1);
-      //     keep = el[i].innerHTML;
-      //     el[i].innerHTML = el[i].innerHTML + "...";
-      //   }
-      //
-      // }
-    // }
-
-
 
     var getPost = function(){
 
       HomeService.get().then(function(result){
-        $scope.articles = result.articles;
-        // $scope.story = result.data.story;
-
-        // setTimeout(function(){
-          // ellipsizeTextBox('article-description');
-
-        // }, 500)
+        $scope.articles = result[0].articles;
+        console.log('CTRL', result);
       })
 
     };
--- public/js/services/HomeService.js
+++ public/js/services/HomeService.js
@@ -1,75 +1,76 @@
 thisApp
-.factory('HomeService', function($http){
-console.log('SERVICE');
-  return {
-
-    get: function(){
-      console.log('CHECK');
-      return $http.get('http://ec2-35-165-125-36.us-west-2.compute.amazonaws.com/stories/json')
-      .then(
-        function(success){
-          console.log('success1', success);
-          return success.data[0];
-        },
-        function(error){
-          console.log('error1', error);
-          return error;
-        }
-      );
-
-    }
-
-  }
+.factory('HomeService', function($http, $q){
+  console.log('SERVICE');
 
 
   // return {
+  //
   //   get: function(){
-  //
-  //     return [
-  //       {
-  //         article_id: 123,
-  //         title: 'The Title 1',
-  //         description: 'This is the description',
-  //         author: 'The Author',
-  //         url: 'https://google.com',
-  //         image: 'http://placehold.it/350x150',
-  //         published: '2017-02-08 02:40:12',
-  //         source: 'the-huffington-post',
-  //         x_score: 25
+  //     console.log('CHECK');
+  //     return $http.get('http://ec2-35-165-125-36.us-west-2.compute.amazonaws.com/stories/json')
+  //     .then(
+  //       function(success){
+  //         console.log('success1', success);
+  //         return success.data[0];
   //       },
-  //       {
-  //         article_id: 111,
-  //         title: 'The Title 2',
-  //         description: 'This is the description',
-  //         author: 'The Author',
-  //         url: 'https://google.com',
-  //         image: 'http://placehold.it/350x150',
-  //         published: '2017-02-08 02:40:12',
-  //         source: 'the-huffington-post',
-  //         x_score: 75
-  //       },
-  //       {
-  //         article_id: 222,
-  //         title: 'The Title 3',
-  //         description: 'This is the description',
-  //         author: 'The Author',
-  //         url: 'https://google.com',
-  //         image: 'http://placehold.it/350x150',
-  //         published: '2017-02-08 02:40:12',
-  //         source: 'the-huffington-post',
-  //         x_score: 50
+  //       function(error){
+  //         console.log('error1', error);
+  //         return error;
   //       }
-  //     ]
-  //
+  //     );
   //
   //   }
+  //
   // }
 
 
+  return {
+    get: function(){
+
+      var allStories = [];
+
+      var i = 0;
+      var defer = $q.defer();
+
+      var makeStories = function(id){
+
+        if (id >= 9) {
+
+          defer.resolve(allStories);
+
+        }else{
+
+          return $http.get('http://ec2-35-165-125-36.us-west-2.compute.amazonaws.com/stories/json')
+          .then(
+            function(success){
+              console.log('success1', success.data[0].articles);
+
+              allStories.push({
+                articles: success.data[0].articles,
+                storyTitle: success.data[0].title + id,
+                date: "2017-03-11 18:02:37",
+                featured_image: success.data[0].featured_image + ''
+              });
+
+              i++;
+              makeStories(i);
+
+            });
+          }
+        }
+
+        makeStories(i);
+        return defer.promise;
+
+
+      }
+    }
 
 
 
 
 
 
-});
+
+
+  });
